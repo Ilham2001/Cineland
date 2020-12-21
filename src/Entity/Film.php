79a5a8984a08d7e74ce4a8c2ccafbc1b3b8c4288
ilham <ilham.lazar.01@gmail.com>
2020-12-21 15:47:37 +0100

    Entity Film created

---
+++ src/Entity/Film.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FilmRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=FilmRepository::class)
+ */
+class Film
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="time")
+     * @Assert\Unique
+     */
+    private $duree;
+
+    /**
+     * @ORM\Column(type="date")
+     * @Assert\LessThan("today")
+     */
+    private $dateSortie;
+
+    /**
+     * @ORM\Column(type="float")
+     * @Assert\GreaterThan(0)
+     * @Assert\LessThan(20)
+     */
+    private $note;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\Positive
+     */
+    private $ageMinimal;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDuree(): ?\DateTimeInterface
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(\DateTimeInterface $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getDateSortie(): ?\DateTimeInterface
+    {
+        return $this->dateSortie;
+    }
+
+    public function setDateSortie(\DateTimeInterface $dateSortie): self
+    {
+        $this->dateSortie = $dateSortie;
+
+        return $this;
+    }
+
+    public function getNote(): ?float
+    {
+        return $this->note;
+    }
+
+    public function setNote(float $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getAgeMinimal(): ?int
+    {
+        return $this->ageMinimal;
+    }
+
+    public function setAgeMinimal(int $ageMinimal): self
+    {
+        $this->ageMinimal = $ageMinimal;
+
+        return $this;
+    }
+}
